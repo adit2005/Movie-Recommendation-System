b7df1febef76a980c953ba30bbd5588589942682
Aditya Singh Chauhan <adityaschauhan346@gmail.com>
2024-04-14 19:45:22 +0530

    Update web_application_code.py

--- web_application_code.py
+++ web_application_code.py
@@ -3,7 +3,7 @@ import pickle
 import pandas as pd
 import requests
 def fetch_poster(movie_id):
-    url = "https://api.themoviedb.org/3/movie/{}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US".format(movie_id)
+    url = "https://api.themoviedb.org/3/movie/{}?api_key=2b0dfe0193d8f6047afcea92b63a6f6b8&language=en-US".format(movie_id)
     data = requests.get(url)
     data = data.json()
     poster_path = data['poster_path']
